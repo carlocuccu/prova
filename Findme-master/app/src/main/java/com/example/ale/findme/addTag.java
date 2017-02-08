0fad101e506708c5885b5b606f7c7001f1b28da7
carlocuccu <carlocuccu@91.it>
2017-02-08 10:35:46 +0100

    Test

---
+++ Findme-master/app/src/main/java/com/example/ale/findme/addTag.java
@@ -0,0 +1,22 @@
+package com.example.ale.findme;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+public class addTag extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tag);
+
+//fjfnf
+
+
+    }
+}
